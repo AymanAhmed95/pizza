9d035b79b319d3bf901586cfdb61511f1d05f0a4
Ayman Ahmed <only.developing95@gmail.com>
2020-07-13 15:07:16 +0200

    add tests

--- pizzaApp/tests.py
+++ pizzaApp/tests.py
@@ -1,8 +1,15 @@
-from django.test import TestCase
-
 # Create your tests here.
+from rest_framework.status import HTTP_200_OK
+
 from .constants import ORDER_STATUS_PENDING
 from .models import Order, Customer, Pizza, OrderPizzaRel
+from django.test import TestCase, Client
+from pprint import pprint
+
+from .serializers import OrderSerializer
+from rest_framework.test import APIRequestFactory, APITestCase
+
+from .views import OrderViewSet
 
 
 class OrderTest(TestCase):
@@ -22,6 +29,16 @@ class OrderTest(TestCase):
 
         db_order = Order.objects.latest('created')
         self.assertEqual(order.id, db_order.id)
+
+
+class ApiTest(APITestCase):
+    def test_get_orders_api(self):
+        factory = APIRequestFactory()
+        orderView = OrderViewSet.as_view({'get': 'list'})
+        request = factory.get(path='/orders', content_type='application/json')
+        orders = orderView(request)
+        self.assertEqual(orders.status_code, HTTP_200_OK)
+        self.assertEqual(orders.data, OrderSerializer(Order.objects.all(), many=True).data)
 
 
 def get_test_user():
